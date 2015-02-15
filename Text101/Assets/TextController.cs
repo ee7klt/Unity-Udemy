8a3f7ea990231da5358d542483c2fe6cf3d1f80b
ee7klt <kltah1@gmail.com>
2015-02-15 17:21:43 +0800

    adding text and changing text

---
+++ Text101/Assets/TextController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TextController : MonoBehaviour {
+
+
+
+
+
+	public Text text;
+	// Use this for initialization
+	void Start () {
+		text.text = "Hello world";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
